e2f52c942faa54773073cacdb28b6d161ce702f5
milatikhanifah <milala2275@gmail.com>
2025-10-28 10:45:26 +0700

    Create variable.php

---
+++ praktik1/Latihan/variable.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Contoh Script PHP</title>
+</head>
+<body>
+    <?php
+    $nama = "Milati Khanifah";
+    echo "$nama berkata \"Katakan namamu!\"";
+    ?>
+</body>
+</html>
